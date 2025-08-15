73bd6c027bc5ae51ea92828bd0ccb978344b7ab8
Chris Miles <chrismiles@apple.com>
2025-08-15 16:53:03 -0700

    Cleans up cmake files.

--- cmake/modules/shared/CompilerSettings.cmake
+++ cmake/modules/shared/CompilerSettings.cmake
@@ -9,33 +9,8 @@
 # Analogous to project-level build settings in an Xcode project.
 add_compile_options(
   "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-package-name org.swift.play>")
-#add_compile_options(
-#  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -require-explicit-sendable>")
-#add_compile_options(
-#  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-experimental-feature -Xfrontend AccessLevelOnImport>")
-#add_compile_options(
-#  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-upcoming-feature -Xfrontend ExistentialAny>"
-#  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-upcoming-feature -Xfrontend InternalImportsByDefault>"
-#  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-upcoming-feature -Xfrontend MemberImportVisibility>"
-#  "SHELL:$<$<COMPILE_LANGUAGE:Swift>:-Xfrontend -enable-upcoming-feature -Xfrontend InferIsolatedConformances>")
 
 # Platform-specific definitions.
 if(APPLE)
   add_compile_definitions("SWP_TARGET_OS_APPLE")
 endif()
-#set(SWP_NO_EXIT_TESTS_LIST "iOS" "watchOS" "tvOS" "visionOS" "WASI" "Android")
-#if(CMAKE_SYSTEM_NAME IN_LIST SWP_NO_EXIT_TESTS_LIST)
-#  add_compile_definitions("SWP_NO_EXIT_TESTS")
-#endif()
-#set(SWP_NO_PROCESS_SPAWNING_LIST "iOS" "watchOS" "tvOS" "visionOS" "WASI" "Android")
-#if(CMAKE_SYSTEM_NAME IN_LIST SWP_NO_PROCESS_SPAWNING_LIST)
-#  add_compile_definitions("SWP_NO_PROCESS_SPAWNING")
-#endif()
-#if(NOT APPLE)
-#  add_compile_definitions("SWP_NO_SNAPSHOT_TYPES")
-#  add_compile_definitions("SWP_NO_FOUNDATION_FILE_COORDINATION")
-#endif()
-#if(CMAKE_SYSTEM_NAME STREQUAL "WASI")
-#  add_compile_definitions("SWP_NO_DYNAMIC_LINKING")
-#  add_compile_definitions("SWP_NO_PIPES")
-#endif()
