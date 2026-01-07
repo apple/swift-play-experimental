942fa55d7d5da0e24af71e4c783bae9e8a62542c
Chris Miles <chrismiles@apple.com>
2026-01-07 09:49:33 -0800

    Documents the Playground macro.

--- Sources/Playgrounds/Playgrounds.swift
+++ Sources/Playgrounds/Playgrounds.swift
@@ -7,6 +7,16 @@
 // See https://swift.org/LICENSE.txt for license information
 //
 
+/// Declares a runnable playground block that can be discovered and executed by tools like "swift play".
+///
+/// The `#Playground` macro creates a discoverable code block that can be run independently
+/// from the main program execution. Playgrounds are useful for exploration, experimentation,
+/// and demonstration code that can be executed on-demand.
+///
+/// - Parameters:
+///   - name: An optional string that provides a display name for the playground.
+///           If `nil`, the playground will be unnamed but still discoverable.
+///   - body: A closure containing the code to execute when the playground is run.
 @freestanding(declaration)
 public macro Playground(
   _ name: String? = nil,
